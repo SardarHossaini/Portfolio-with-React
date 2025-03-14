aaed48e577d21d0196bee3376f54da7a006f997e
Sardar219 <sardarhossaini2022@gmail.com>
2025-03-14 22:52:50 +0430

    added active functionalty for navbar items

--- src/component/Navbar/Navbar.jsx
+++ src/component/Navbar/Navbar.jsx
@@ -1,16 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Navbar.css";
 
 export const Navbar = () => {
+  const [menu, setMenu] = useState("contact");
   return (
     <div className="navbar">
       <h2>SarDar</h2>
       <ul className="nav-menu">
-        <li>Home</li>
-        <li>About Me</li>
-        <li>Services</li>
-        <li>Portfolio</li>
-        <li>Contact</li>
+        <li>
+          <p
+            onClick={() => setMenu("home")}
+            className={menu === "home" ? "active" : ""}
+          >
+            Home
+          </p>
+        </li>
+        <li>
+          <p
+            onClick={() => setMenu("about")}
+            className={menu === "about" ? "active" : ""}
+          >
+            About Me
+          </p>
+        </li>
+        <li>
+          <p
+            onClick={() => setMenu("services")}
+            className={menu === "services" ? "active" : ""}
+          >
+            Services
+          </p>
+        </li>
+        <li>
+          <p
+            onClick={() => setMenu("work")}
+            className={menu === "work" ? "active" : ""}
+          >
+            Portfolio
+          </p>
+        </li>
+        <li>
+          <p
+            onClick={() => setMenu("contact")}
+            className={menu === "contact" ? "active" : ""}
+          >
+            Contact
+          </p>
+        </li>
       </ul>
       <div className="nav-connect">Connect With Me</div>
     </div>
